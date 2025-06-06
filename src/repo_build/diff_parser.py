f8dfc5b2485b70f6340efa868f9168e19073e483
Bryton Lee <brl013@ucsd.edu>
2025-06-06 03:42:05 +0000

    updated parser

--- src/repo_build/diff_parser.py
+++ src/repo_build/diff_parser.py
@@ -2,13 +2,14 @@ import re
 import lib.diff_util as DIFF_UTIL
 import argparse
 
-parser = argparse.ArgumentParser(description="Script that takes diff file name")
+parser = argparse.ArgumentParser(description="Script that takes diff file path and output file path")
 parser.add_argument("inputs", nargs=1, type=str, help="1 input string")
+parser.add_argument("outputs", nargs=1, type=str, help="1 output string")
 args = parser.parse_args()
-input1 = args.inputs[0]
-print(input1)
-diff_file_path = input1
-with open(diff_file_path, 'r') as f:
-    diff_content = f.read()
+input_path, output_path = args.inputs
+print(input_path)
+# diff_file_path = input1
+# with open(diff_file_path, 'r') as f:
+#     diff_content = f.read()
 
-DIFF_UTIL.parse_diffoscope_output(diff_content, "diff_summary.txt")
+DIFF_UTIL.parse_diffoscope_output(input_path, output_path)
